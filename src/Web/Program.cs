9f11c5359afbb02fe12af55345152e93231e3312
Sergio Hernandez <sergio.hernandez.p@gmail.com>
2024-11-25 13:44:33 -0500

    Moved Allowed CORS Origins to the config file

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,10 +1,13 @@
-using System.Reflection;
+using Ardalis.GuardClauses;
 using Common.Application;
 using TrackHub.Manager.Infrastructure.ManagerDB;
 using TrackHub.Manager.Web.GraphQL.Mutation;
 using TrackHub.Manager.Web.GraphQL.Query;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var allowedCORSOrigins = builder.Configuration.GetSection("AllowedCorsOrigins").Get<string>();
+Guard.Against.Null(allowedCORSOrigins, message: $"Allowed Origins configuration for CORS not loaded");
 
 // Add services to the container.
 builder.Services.AddApplicationServices();
@@ -26,10 +29,18 @@ builder.Services
 builder.Services.AddCors(options => options
     .AddPolicy("AllowFrontend",
         builder => builder
-                    .WithOrigins("https://localhost:3000")
+                    .WithOrigins(allowedCORSOrigins)
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials()));
+
+// Configure HSTS
+builder.Services.AddHsts(options =>
+{
+    options.MaxAge = TimeSpan.FromDays(365 * 2);
+    options.IncludeSubDomains = true;
+    options.Preload = true;
+});
 
 var app = builder.Build();
 
@@ -41,7 +52,6 @@ app.UseCors("AllowFrontend");
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -50,7 +60,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseExceptionHandler(options => { });
-app.MapEndpoints(Assembly.GetExecutingAssembly());
 
 app.MapGraphQL();
 
