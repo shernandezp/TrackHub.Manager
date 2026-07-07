6fed61da2202dfd00584cfe6117c9463ae8b3f0f
Sergio Hernandez <sergio.hernandez.p@gmail.com>
2026-07-06 22:20:47 -0400

    Fix naming convention

--- src/Web/GraphQL/Mutation/DocumentMutation.cs
+++ src/Web/GraphQL/Mutation/DocumentMutation.cs
@@ -8,5 +8,5 @@ public partial class Mutation
     public async Task<bool> MarkDocumentUploaded([Service] ISender sender, MarkDocumentUploadedCommand command) { await sender.Send(command); return true; }
     public async Task<bool> MarkDocumentScanResult([Service] ISender sender, MarkDocumentScanResultCommand command) { await sender.Send(command); return true; }
     public async Task<bool> ExpireDocument([Service] ISender sender, ExpireDocumentCommand command) { await sender.Send(command); return true; }
-    public async Task<bool> DeleteDocumentReference([Service] ISender sender, DeleteDocumentReferenceCommand command) { await sender.Send(command); return true; }
+    public async Task<Guid> DeleteDocumentReference([Service] ISender sender, DeleteDocumentReferenceCommand command) { await sender.Send(command); return command.DocumentId; }
 }
